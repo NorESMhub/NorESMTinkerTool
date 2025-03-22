b1db4e99cbc32cc33f6febd403f957483a731476
Ove <ovehaugv@outlook.com>
2025-03-22 19:17:00 +0100

    Make changes to support writing to ini file

--- scripts/aerosol_ppe_cam_nl_updates.py
+++ scripts/aerosol_ppe_cam_nl_updates.py
@@ -6,8 +6,8 @@ import pkg_resources
 import copy
 
 config_path = pkg_resources.resource_filename('config','default_control_atm.ini')
-station_csv = pkg_resources.resource_filename(__name__, '../input_files/stations_combined.csv')
-fincl_csv = pkg_resources.resource_filename(__name__, '../input_files/output_variables.csv')
+station_csv = pkg_resources.resource_filename('input_files', 'stations_combined.csv')
+fincl_csv = pkg_resources.resource_filename('input_files', 'output_variables.csv')
 
 def read_config(config_file):
     with open(config_file) as f:
@@ -31,6 +31,7 @@ def main():
                         help="Overwrite the existing fincl defined in the control atm file")
     parser.add_argument("--output-file","-of", type=str, default="./aerosol_ppe_control_atm.ini",
                         help="Path to the where to write the update control atm file")
+    parser.add_argument("--pertape-flags","-pf",nargs='+',type=str, default=['A','I'])
     args = parser.parse_args()
 
     
@@ -43,16 +44,43 @@ def main():
         station_data_string = write_out_station_nm_string(station_csv)
     if args.fincl_data_file:
         namelist_string = get_namlist_string('mon-global',1,args.fincl_data_file,'A')
+        station_data_namlist_vars = get_namlist_string('3-h-station',2,args.fincl_data_file,'I')
     else:
         namelist_string = get_namlist_string('mon-global',1,fincl_csv, 'A')
+        station_data_namlist_vars = get_namlist_string('3-h-station',2,fincl_csv, 'I')
 
     if args.control_atm_file:
         config_path = args.control_atm_file
     else:
         config_path = config_path
     config = read_config(config_path)
-    
-    import IPython; IPython.embed()
+    nml_variable = namelist_string.split('=')[0].strip()
+    station_nml_varialbe = station_data_namlist_vars.split('=')[0].strip()
+    nml_from_config = config['camexp'].get(nml_variable, None)
+    # Merge namelist_string with name list from control atm file
+    for nml_n,nml_v in zip([nml_variable, station_nml_varialbe], [namelist_string, station_data_namlist_vars]):
+        nml_from_config = config['camexp'].get(nml_n, None)
+        if nml_from_config is not None and args.overwrite_existing_fincl == False:
+            nml_config_vars = nml_from_config.split('\n')
+            namelist_string_vars = nml_v.split('=')[1].strip().split('\n')
+            # merge the two lists ensuring no duplicates
+            namelist_string_vars = list(set(nml_config_vars + namelist_string_vars))
+            # sort alphabetically
+        else:
+            namelist_string_vars = nml_v
+            namelist_string_vars = namelist_string_vars.split('=')[1].strip().split('\n')
+        namelist_string_vars.sort()
+            # updated nml to config 
+        config['camexp'][nml_n] = ('\n').join(namelist_string_vars)
+        # write the station data string to the control atm file
+
+    station_variable_name_str = station_data_string.split('=')[0].strip()
+    station_variables_str = station_data_string.split('=')[1].strip()
+    config['camexp'][station_variable_name_str] = station_variables_str
+    # Write update control atm file
+    config['camexp']['avgflag_pertape'] = ", ".join(args.pertape_flags)
+    with open(args.output_file, 'w') as f:
+        config.write(f)
 
 
 if __name__ == '__main__':
