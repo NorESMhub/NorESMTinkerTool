5ec76e5cf6ee99702b54f0346a174eac8d51c064
maritsandstad <marit.sandstad@astro.uio.no>
2025-11-03 13:29:33 +0100

    Initial commit skeleton

---
+++ tinkertool/utils/make_land_parameterfiles.py
@@ -0,0 +1,10 @@
+import xarray as xr
+
+def make_new_ctsm_pamfile(pam_change_dict, orig_pamfile = ""):
+    pass
+
+def make_new_fates_pamfile(pam_change_dict, orig_pamfile = ""):
+    pass
+
+if __name__ == '__main__':
+    print("Add in tests")
