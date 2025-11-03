825f94687b57bfd4fe40cd96ee450ffb1f312fa5
Ove <ovehaugv@outlook.com>
2025-11-03 21:09:00 +0100

    make top functions more easily available

--- tinkertool/scripts/generate_paramfile/__init__.py
+++ tinkertool/scripts/generate_paramfile/__init__.py
@@ -1,2 +1,4 @@
-# --- constants
+from .config import ParameterFileConfig
+from .generate_paramfile import generate_paramfile
 PARAMFILE_INPUT_TYPES = ['user_nl', 'CTSM_param_file', 'FATES_param_file']
+__all__ = ["ParameterFileConfig", "generate_paramfile"]
