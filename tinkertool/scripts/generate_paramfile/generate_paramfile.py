3c3ea1a1cf3b1339266917e1b1569b80364d88fb
Johannesfjeldsaa <98762880+Johannesfjeldsaa@users.noreply.github.com>
2025-11-04 21:38:53 +0100

    system for CTSM_param_file and FATES_param_file marked parameters. Will take variable values and perturb a template pam_file, return a path and put that path as a step in for individual user_nl entries. This path is added to user_nl_clm during building

--- tinkertool/scripts/generate_paramfile/generate_paramfile.py
+++ tinkertool/scripts/generate_paramfile/generate_paramfile.py
@@ -170,23 +170,126 @@ def generate_paramfile(config: ParameterFileConfig):
                 log_info_detailed('tinkertool_log', f"{outfile} generated with SOA_y_scale_chem_mech_in = {scale_factor}")
             del sample_points["SOA_y_scale_chem_mech_in"]
 
-    logging.debug("Creating xarray dataset")
-    out_ds = xr.Dataset(
-        data_vars = sample_points,
-        coords={'nmb_sim':config.nmb_sim_dim})
+    sample_points_with_files = sample_points.copy()
 
-    for param in out_ds.data_vars:
-        out_ds[param].attrs['description'] = config.param_ranges[param].get('description', 'No description available')
-        out_ds[param].attrs['default'] = config.param_ranges[param].get('default', 'No default value available')
-        out_ds[param].attrs['sampling'] = config.param_ranges[param].get('sampling', 'No sampling method available')
-        out_ds[param].attrs['esm_component'] = config.param_ranges[param].get('component', config.assumed_esm_component).lower()
+    # create sample_points taking into account input_type, that is:
+    # * if input_type is 'user_nl' we do not need to change anything
+    # * if input_type is 'CTSM_param_file' or 'FATES_param_file'
+    #   we need to create edited copies of the respective default *_param_file.
+    #   If there are multiple such parameters, we bunch them together to
+    #   create one edited copy per simulation.
+    ctsm_fates_paramfile_info = []
+    if checked_config.change_ctsm_params:
+        CTSM_param_file_params = [
+            param for param in checked_config.params
+            if safe_get_param_value(checked_config.param_ranges[param], "input_type") == "CTSM_param_file"
+        ]
+        ctsm_fates_paramfile_info.append(("CTSM_param_file", 'paramfile', CTSM_param_file_params, make_new_ctsm_pamfile))
+    if checked_config.change_fates_params:
+        FATES_param_file_params = [
+            param for param in checked_config.params
+            if safe_get_param_value(checked_config.param_ranges[param], "input_type") == "FATES_param_file"
+        ]
+        ctsm_fates_paramfile_info.append(("FATES_param_file", 'fates_paramfile', FATES_param_file_params, make_new_fates_pamfile))
+
+    if ctsm_fates_paramfile_info:
+        logging.debug("Generating land model parameter files")
+        for param_type, user_nl_kw, param_group, make_new_param_file in ctsm_fates_paramfile_info:
+            # create new section in the ConfigParser object (if it doesn't exist)
+            if not checked_config.param_ranges.has_section(user_nl_kw):
+                checked_config.param_ranges.add_section(user_nl_kw)
+            pg_param_ranges = checked_config.param_ranges[user_nl_kw]
+
+            pam_filepaths = []
+            for sim_indx in checked_config.nmb_sim_dim:
+                pam_change_dict = {}
+                for param in param_group:
+                    pdata = checked_config.param_ranges[param]
+                    pam_change_dict[param] = sample_points[param][1][sim_indx]  # Fixed: access tuple correctly
+                if pam_change_dict:
+                    if param_type == 'CTSM_param_file':
+                        orig_pamfile = checked_config.ctsm_default_param_file
+
+                    elif param_type == 'FATES_param_file':
+                        orig_pamfile = checked_config.fates_default_param_file
+                        if orig_pamfile is None:
+                            raise ValueError("FATES default parameter file path is not set in the configuration:\n"
+                                            "Please provide a valid path to the FATES default parameter file.")
+                    else:
+                        raise ValueError(f"Unknown parameter file type '{param_type}'.")
+                    save_path = checked_config.tinkertool_output_dir.joinpath(
+                        user_nl_kw,
+                        f"{user_nl_kw}_file_sim{sim_indx}.nc"
+                    ).resolve()
+                    # Ensure directory exists before creating the file
+                    save_path.parent.mkdir(parents=True, exist_ok=True)
+                    new_pamfile = make_new_param_file(
+                        pam_change_dict,
+                        orig_pamfile=orig_pamfile,
+                        file_dump=save_path
+                    )
+                    pam_filepaths.append(str(new_pamfile))
+                    log_info_detailed('tinkertool_log', f"{new_pamfile} generated with changes: {pam_change_dict}")
+            if pam_filepaths:
+                sample_points_with_files[user_nl_kw] = (['nmb_sim'], pam_filepaths)
+                # add attribute info
+                pg_defaults, pg_newvals, pg_sampling, pg_input_type, pg_interdependent_with, pg_esm_component = {}, {}, {}, {}, {}, {}
+                for param in param_group:
+                    pdata = checked_config.param_ranges[param]
+                    pg_defaults[param] = pdata['default']
+                    pg_newvals[param] = sample_points[param][1].copy()  # Fixed: access tuple correctly
+                    pg_sampling[param] = safe_get_param_value(pdata, 'sampling', 'No sampling method available')
+                    pg_input_type[param] = safe_get_param_value(pdata, 'input_type', '')
+                    pg_interdependent_with[param] = safe_get_param_value(pdata, 'interdependent_with', '')
+                    pg_esm_component[param] = safe_get_param_value(pdata, 'component', '')
+                    del sample_points_with_files[param]
+                pg_param_ranges['default'] = ",\n".join([f"{param}: {pg_defaults[param]}" for param in param_group])
+                pg_param_ranges['sampling'] = ",\n".join([f"{param}: {pg_sampling[param]}" for param in param_group])
+                # input_type should be same for all params in group
+                input_type = [pg_input_type[param] for param in param_group]
+                if not all(it == input_type[0] for it in input_type):
+                    raise ValueError(f"Parameters in group {param_group} have differing input_types: {input_type}")
+                pg_param_ranges['input_type'] = input_type[0]
+                pg_param_ranges['interdependent_with'] = ",\n".join([f"{param}: {pg_interdependent_with[param]}" for param in param_group])
+                pg_param_ranges['esm_component'] = ",\n".join([f"{param}: {pg_esm_component[param]}" for param in param_group])
+                pg_param_ranges['description'] = f"{param_type} file perturbing parameters:\n" + \
+                    ",\n".join([f"{param}, default -> new value: {pg_defaults[param]} -> {pg_newvals[param]}" for param in param_group])
+
+    logging.debug("Creating xarray dataset")
+    raw_ds = xr.Dataset(
+        data_vars = sample_points,
+        coords={'nmb_sim':checked_config.nmb_sim_dim}
+    )
+    out_ds = xr.Dataset(
+        data_vars = sample_points_with_files,
+        coords={'nmb_sim':checked_config.nmb_sim_dim}
+    )
+
+    for ds in [raw_ds, out_ds]:
+        for param in ds.data_vars:
+            pdata = checked_config.param_ranges[str(param)]
+            ds[param].attrs['description'] = safe_get_param_value(pdata, 'description', 'No description available')
+            ds[param].attrs['default'] = safe_get_param_value(pdata, 'default', 'No default value available')
+            ds[param].attrs['sampling'] = safe_get_param_value(pdata, 'sampling', 'No sampling method available')
+
+            input_type = safe_get_param_value(pdata, 'input_type', "")
+            assert not isinstance(input_type, str) or input_type.lower() in [it.lower() for it in PARAMFILE_INPUT_TYPES], \
+                f"Invalid input type '{input_type}, type({type(input_type)})' for parameter '{param}'. Supported types are: {PARAMFILE_INPUT_TYPES}."
+            ds[param].attrs['input_type'] = input_type
+            ds[param].attrs['interdependent_with'] = safe_get_param_value(pdata, "interdependent_with", "")
+
+            component = safe_get_param_value(pdata, 'component', checked_config.assumed_esm_component)
+            ds[param].attrs['esm_component'] = component.lower() if component else checked_config.assumed_esm_component.lower()
 
     # Add variables with irregular names
-    if config.change_chem_mech:
+    if checked_config.change_chem_mech:
         out_ds['chem_mech_in'] = (['nmb_sim'], chem_mech_in)
     current_time = datetime.now().replace(microsecond=0)
     # Assigning to your attribute
-    out_ds.attrs['created'] = f"Created " + current_time.strftime('%Y-%m-%d %H:%M:%S')
-    out_ds.to_netcdf(config.param_sample_outpath)
+    for ds in [raw_ds, out_ds]:
+        ds.attrs['created'] = f"Created " + current_time.strftime('%Y-%m-%d %H:%M:%S')
+    raw_ds.to_netcdf(checked_config.param_sample_outpath.with_suffix('.raw.nc'))
+    out_ds.to_netcdf(checked_config.param_sample_outpath.with_suffix('.nc'))
 
-    logging.info(f">> Parameter file {config.param_sample_outpath} generated successfully.")
+    logging.info(f">> Raw parameter file (only numbers) {checked_config.param_sample_outpath.with_suffix('.raw.nc')} generated successfully.")
+    logging.info(f">> Parameter file (with filepaths) {checked_config.param_sample_outpath.with_suffix('.nc')} generated successfully.")
