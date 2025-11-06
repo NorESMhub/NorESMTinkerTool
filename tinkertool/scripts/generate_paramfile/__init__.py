5dffe19b5b383281e161d96d3af1938a2471b5e9
Ove <ovehaugv@outlook.com>
2025-11-06 16:21:37 +0100

    Refactor parameter file generation logic and enhance configuration handling
    - Changes are to the best of my ability brought in from general_purpose_4_noresm3
    - TODO take suggestions from johannes

--- tinkertool/scripts/generate_paramfile/__init__.py
+++ tinkertool/scripts/generate_paramfile/__init__.py
@@ -1,4 +1,4 @@
 from .config import ParameterFileConfig
 from .generate_paramfile import generate_paramfile
-__all__ = ["ParameterFileConfig", "generate_paramfile"]
 PARAMFILE_INPUT_TYPES = ['user_nl', 'CTSM_param_file', 'FATES_param_file']
+__all__ = ["ParameterFileConfig", "generate_paramfile"]
--- tinkertool/scripts/generate_paramfile/main.py
+++ tinkertool/scripts/generate_paramfile/main.py
@@ -1,11 +1,9 @@
 from tinkertool.scripts.generate_paramfile.config import ParameterFileConfig
 from tinkertool.scripts.generate_paramfile.generate_paramfile import generate_paramfile
 
-
 def main():
     config = ParameterFileConfig.from_cli()    # type: ignore  # Suppress Pylance warning
     generate_paramfile(config)
-
 
 if __name__ == "__main__":
     main()
