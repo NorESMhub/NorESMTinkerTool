0af5b30b05306eb5cd62a1504e34527e677056fe
Ove <ovehaugv@outlook.com>
2026-01-09 15:56:17 +0100

    make generate_paramfile available on the top level

--- tinkertool/scripts/generate_paramfile/__init__.py
+++ tinkertool/scripts/generate_paramfile/__init__.py
@@ -1,2 +1,5 @@
 # --- constants
 PARAMFILE_INPUT_TYPES = ['user_nl', 'CTSM_param_file', 'FATES_param_file']
+
+# make generate_paramfile available at package level
+from tinkertool.scripts.generate_paramfile.generate_paramfile import generate_paramfile
