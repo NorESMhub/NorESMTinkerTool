fe61291703521926d9d895cc30862ef475a5b418
Ove <ovehaugv@outlook.com>
2026-01-13 13:22:24 +0100

    update aerosol ppe usecase

---
+++ usermods/aerosol_ppe/scripts/build_oat_aerosol_ppe.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+from tinkertool.scripts.create_ppe.config import BuildPPEConfig
+from tinkertool.scripts.create_ppe.create_ppe import build_ppe
+
+if __name__ == "__main__":
+    build_ppe_config = BuildPPEConfig(
+        simulation_setup_path='../aerosol_ppe_settings.ini',
+        overwrite=True,
+        build_base_only=True,
+        log_dir                 = Path(__file__).parent.joinpath('../output_files', 'logs').resolve(),
+        log_mode                = 'w'
+    )
+
+    build_ppe(build_ppe_config)
